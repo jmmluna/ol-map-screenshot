d12a31b75fb483715b32389c75b8459980221160
José María Martínez Luna <jmmluna@gmail.com>
2020-04-29 01:07:17 +0200

    Update demo version

---
+++ test/index.js
@@ -0,0 +1,39 @@
+import 'ol/ol.css';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+import olMapScreenshot from '../index';
+
+const map = new Map({
+    layers: [
+        new TileLayer({
+            source: new OSM()
+        })
+    ],
+    target: 'map',
+    view: new View({
+        center: [0, 0],
+        zoom: 2
+    })
+});
+
+(async() => {
+    try {
+        doDonwload('map-screenshot.jpg');
+    } catch (e) {
+        alert("Error: " + e);
+    }
+})();
+
+
+async function doDonwload(fileName) {
+    const response = await olMapScreenshot.getScreenshot(map);
+    const element = document.createElement('a');
+    element.setAttribute('href', response.img);
+    element.setAttribute('download', fileName);
+    element.style.display = 'none';
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+}
